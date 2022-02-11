8c448f51d18e7ed48d5bd49f166db6d6dadbfe51
Yungsun <yungsun3307@163.com>
2022-02-11 09:31:04 +0800

    feat: 🥳 Configured the package file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    cssCodeSplit: true,
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.ts'),
+      name: 'rx-button-next',
+      formats:['umd'],
+      fileName: (format) => `rx-button-next.${format}.js`
+    }
+  }
 })
